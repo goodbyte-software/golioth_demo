9c13449459d91ece06f11b703518feaaf8f9e533
Dzolowski <hubert00115@gmail.com>
2024-10-07 10:39:21 +0200

    Nrfutill device provider-fix

--- .devcontainer/features/gb_commons/install.sh
+++ .devcontainer/features/gb_commons/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 apt-get update
 apt-get install unzip
+apt install libusb-1.0-0
--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-PS1='\[\e[1;31m\]\u@\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ '
-alias ls='ls --color=auto'
-alias west='echo -e "\e[1;33m$(which west)\e[0m" && $(which west)'
-source ~/.bashrc
 west init -l golioth_iot
 west update
 west zephyr-export
 pip install -r deps/zephyr/scripts/requirements.txt
 west completion bash > $HOME/west-completion.bash
+nrfutil install nrf5sdk-tools
+nrfutil install device
 echo 'source $HOME/west-completion.bash' >> $HOME/.bashrc
+echo "  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/^[ *\t]*//' -e 's/^/ (/' -e 's/$/)/'| head" >> ~/.bashrc && \
+echo "}" >> ~/.bashrc && \
+echo 'export PS1="\u@\h \[\033[36m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] # "' >> ~/.bashrc
+
