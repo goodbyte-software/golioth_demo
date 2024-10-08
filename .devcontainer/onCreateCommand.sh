04620d7e880fb76d19e0ea2a16228c9a353a338f
Dzolowski <hubert00115@gmail.com>
2024-10-08 18:40:47 +0200

    Fixed colors in terminal

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -7,7 +7,7 @@ west completion bash > $HOME/west-completion.bash
 nrfutil install nrf5sdk-tools
 nrfutil install device
 echo 'source $HOME/west-completion.bash' >> $HOME/.bashrc
-echo "  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/^[ *\t]*//' -e 's/^/ (/' -e 's/$/)/'| head" >> ~/.bashrc && \
-echo "}" >> ~/.bashrc && \
-echo 'export PS1="\u@\h \[\033[36m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] # "' >> ~/.bashrc
-
+echo 'parse_git_branch() {' >> $HOME/.bashrc
+echo "  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/^[ *\t]*//' -e 's/^/ (/' -e 's/$/)/'| head" >> $HOME/.bashrc
+echo "}" >> $HOME/.bashrc
+echo 'export PS1="\u@\h \[\033[36m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] # "' >> $HOME/.bashrc
