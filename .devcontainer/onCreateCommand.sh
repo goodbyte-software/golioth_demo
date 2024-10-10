a2673666b6e2fa31d7b9df34d22d8fd9ab0f19fe
Krzysztof Kierys <krzysztof.kierys@goodbyte.pl>
2024-10-11 01:28:40 +0200

    Zephyr env variables

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -2,12 +2,19 @@
 west init -l golioth_iot
 west update
 west zephyr-export
+
 pip install -r deps/zephyr/scripts/requirements.txt
-west completion bash > $HOME/west-completion.bash
+
 nrfutil install nrf5sdk-tools
 nrfutil install device
+
+west completion bash > $HOME/west-completion.bash
 echo 'source $HOME/west-completion.bash' >> $HOME/.bashrc
+
 echo 'parse_git_branch() {' >> $HOME/.bashrc
 echo "  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/^[ *\t]*//' -e 's/^/ (/' -e 's/$/)/'| head" >> $HOME/.bashrc
 echo "}" >> $HOME/.bashrc
 echo 'export PS1="\u@\h \[\033[36m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] # "' >> $HOME/.bashrc
+
+echo 'export ZEPHYR_SDK_INSTALL_DIR=/opt/toolchains/zephyr-sdk-0.16.3' >> $HOME/.bashrc
+echo 'source /repo/deps/zephyr/zephyr-env.sh' >> $HOME/.bashrc
