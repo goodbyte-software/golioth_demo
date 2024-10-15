32573c16ea98a66a18300339ca19a60a5c63471c
KiK-GB <krzysztof.kierys@goodbyte.pl>
2024-10-15 18:04:49 +0200

    Clangd feature (#16)
    * Workspace v1.0
    
    * new  docker system
    
    * koniec smiesznych commitow
    
    * Nrfutill device provider-fix
    
    * Fixed colors in terminal
    
    * clangd config
    
    * config clangd in feature
    
    * cleanup
    
    * Zephyr env variables
    
    * config for clangd
    
    ---------
    
    Co-authored-by: Hubert <hubert00115@gmail.com>

--- .devcontainer/features/clangd/install.sh
+++ .devcontainer/features/clangd/install.sh
@@ -4,5 +4,3 @@ wget https://github.com/clangd/clangd/releases/download/19.1.0/clangd-linux-19.1
 unzip clangd.zip -d clangd
 cp clangd/clangd_*/bin/clangd /usr/bin
 cp -r clangd/clangd_*/lib/clang /usr/lib
-
-
